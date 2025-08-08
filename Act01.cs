7ac55d5f731004d795cb6583fa24d2e6bb534a65
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-08-09 06:42:18 +0800

    Add files via upload

---
+++ Act01.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            string input;
+            int countA = 0;
+
+            Console.WriteLine("Enter student names (type 'done' to finish):");
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input.Equals("done", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                names.Add(input);
+            }
+
+            Console.WriteLine("Attendance List in UPPERCASE:");
+            foreach (string name in names)
+            {
+                string upperName = name.ToUpper();
+                Console.WriteLine(upperName);
+
+                if (upperName.StartsWith("A"))
+                    countA++;
+            }
+
+            Console.WriteLine($"\nNumber of names starting with 'A': {countA}");
+        }
+    
+    }
+}
+
