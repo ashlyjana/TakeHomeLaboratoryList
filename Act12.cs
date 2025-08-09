c7532f820396a252bd227e8a87862c4a823d9736
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-08-10 00:45:08 +0800

    Add files via upload

---
+++ Act12.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> productNames = new List<string>();
+            List<int> stockCounts = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter product {i + 1} name: ");
+                productNames.Add(Console.ReadLine());
+
+                Console.Write($"Enter stock count for {productNames[i]}: ");
+                stockCounts.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine("\nUpdated Inventory:");
+            for (int i = 0; i < 10; i++)
+            {
+                string restockFlag = "";
+
+                if (stockCounts[i] < 10)
+                {
+                    stockCounts[i] += 20;
+                    restockFlag = " (Restocked)";
+                }
+
+                Console.WriteLine(productNames[i] + " - " + stockCounts[i] + restockFlag);
+            }
+        }
+    }
+}
