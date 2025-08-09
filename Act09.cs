9d15a2c9be52527ff58625df5833386cd5138e19
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-08-10 00:32:06 +0800

    Add files via upload

---
+++ Act09.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            List<string> formattedNames = new List<string>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("Enter full name:");
+                string name = Console.ReadLine();
+                names.Add(name);
+
+                string[] parts = name.Split(' ');
+                string formattedName = "";
+
+                for (int j = 0; j < parts.Length; j++)
+                {
+                    if (parts[j].Length > 0)
+                    {
+                        formattedName += parts[j].Substring(0, 1).ToUpper() +
+                                         parts[j].Substring(1).ToLower() + " ";
+                    }
+                }
+
+                formattedNames.Add(formattedName.Trim());
+            }
+            Console.WriteLine("Formatted Names:");
+            for (int i = 0; i < formattedNames.Count; i++)
+            {
+                Console.WriteLine(formattedNames[i]);
+            }
+        }
+    }
+}
