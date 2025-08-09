37e457a9704f7b2cb511c92191c69de75b25e2c0
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-08-10 00:40:11 +0800

    Add files via upload

---
+++ Act11.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            List<float> salaries = new List<float>();
+
+            Console.Write("Enter number of employees: ");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"Enter employee {i + 1} name: ");
+                names.Add(Console.ReadLine());
+
+                Console.Write($"Enter employee {i + 1} salary: ");
+                salaries.Add(float.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                if (salaries[i] < 15000)
+                {
+                    salaries[i] += salaries[i] * 0.05f;
+                }
+                else if (salaries[i] >= 15000 && salaries[i] <= 30000)
+                {
+                    salaries[i] += salaries[i] * 0.03f;
+                }
+            }
+
+          
+            Console.WriteLine("\nEmployees earning above 25,000 after adjustment:");
+            for (int i = 0; i < count; i++)
+            {
+                if (salaries[i] > 25000)
+                {
+                    Console.WriteLine(names[i] + " - " + salaries[i]);
+                }
+            }
+            }
+    }
+}
